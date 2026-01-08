b5515e5c6d728fcf4bbb2496bf129e5d77e202b0
Pipe Bustos <pipebustos10@gmail.com>
2026-01-07 23:28:37 -0500

    Day 1 - Java variables and data types

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    public  static void main (String[]args) {
+        String name = "Pipe";
+        int age = 25;
+        double height = 1.84;
+        boolean isStudent = true;
+        String country = "colombia";
+        String sexo ="Masculino";
+
+        System.out.println("name"+ " " + name );
+        System.out.println("age" + " " + age );
+        System.out.println("height" + " " + height );
+        System.out.println("isStudent" + " " + isStudent );
+        System.out.println("country" + " " + country );
+        System.out.println("sexo" + " " + sexo );
+
+        System.out.println("In 5 years, I wil be"+ (age + " " + 5));
+
+
+
+
+
+    }
+}
