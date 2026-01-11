d2425e262caf1a84b955c3408559a37411aa1094
Pipe Bustos <pipebustos10@gmail.com>
2026-01-10 23:50:52 -0500

    Day 3 - for and while loops

---
+++ src/WhileLoopExample.java
@@ -0,0 +1,11 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+
+        int counter = 5;
+
+        while (counter >= 1) {
+            System.out.println("Counter: " + counter);
+            counter--;
+        }
+    }
+}
